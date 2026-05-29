c2b14009810d390f3a6972301ecc3a95367e25c0
emmanuelnwujuchibuike <emmanuelnwujuchibuike@gmail.com>
2026-05-29 09:24:12 -0700

    aurumdone

--- supabase/functions/new-user-handler/index.ts
+++ supabase/functions/new-user-handler/index.ts
@@ -7,7 +7,7 @@ const corsHeaders = {
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
-serve(async (req: Request) => {
+serve async (req: Request) => 
   if (req.method === "OPTIONS") {
     return new Response("ok", { headers: corsHeaders });
   }
